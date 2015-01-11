564193a03944127ab955769f1849fa0f23424884
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2015-01-11 17:42:06 -0600

    Agregar archivos en evaluados

--- controllers/evaluadores.js
+++ controllers/evaluadores.js
@@ -18,6 +18,74 @@ exports.addEvaluadores = function(req, res, next) {
 };
 
 
+//update
+exports.updateEvaluador = function(req, res, next) {
+  // console.log(req);
+  evaluadores.findOneAndUpdate({_id:req.params.id}, req.body, function (err, evaluador) {
+    if(err) res.send(500, err.message);
+    // console.log(accion);
+    res.evaluadores = evaluador;
+    return next();
+  });
+};
+
+
+
+exports.updateEvaluadorFiles = function(req, res, next) {
+
+  if(req.files.cedula_profesional){
+    // RUTA DE ARCHIVOS TEMPORALES
+    var temp_file = config.root+ "/temp/uploads/evaluadores/files/"+req.files.cedula_profesional.name;
+    // RUTA PARA NUEVOS ARCHIVOS
+    var new_file = config.root+ "/uploads/evaluadores/formacion_academica/"+req.params.id+"/cedula_profesional."+req.files.cedula_profesional.extension;
+    req.body.cedula_profesional = new_file;
+
+    // Se mueve el archivo temporal
+    fs.move(temp_file, new_file, function(err) {
+      if (err) return console.error(err);
+      console.log("success!");
+
+    });
+
+
+  }else{
+    console.log("no archivo");
+  }
+
+
+  if(req.files.documentacion_probatoria){
+    // RUTA DE ARCHIVOS TEMPORALES
+    var temp_file2 = config.root+ "/temp/uploads/evaluadores/files/"+req.files.documentacion_probatoria.name;
+    // RUTA PARA NUEVOS ARCHIVOS
+    var new_file2 = config.root+ "/uploads/evaluadores/formacion_academica/"+req.params.id+"/documentacion_probatoria."+req.files.documentacion_probatoria.extension;
+    req.body.documentacion_probatoria = new_file2;
+    // Se mueve el archivo temporal
+    fs.move(temp_file2, new_file2, function(err) {
+      if (err) return console.error(err);
+      console.log("success!");
+    });
+
+
+  }else{
+    console.log("no archivo");
+  }
+
+
+
+  // console.log("**********************");
+  // console.log(req.body);
+
+  evaluadores.findOneAndUpdate({_id:req.params.id}, req.body, function (err, evaluador) {
+    if(err) res.send(500, err.message);
+    // console.log(accion);
+    res.evaluadores = evaluador;
+    return next();
+  });
+};
+
+
+
+
 //Informacion de un un solo evaluador
 exports.findById = function(req, res, next) {
   evaluadores.findById(req.params.id, function(err, evaluador) {
